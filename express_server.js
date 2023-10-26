e98cce748dc190491c477584cf860670362f23ea
svaronc <sebastianvaron96@gmail.com>
2023-10-26 00:54:27 +0000

    create the folder views and create the path to urls to render

--- express_server.js
+++ express_server.js
@@ -9,15 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>world</b></body></html>\n");
-});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
