c35ab8fd551c1e148fad7a3eb4a9375993528759
svaronc <sebastianvaron96@gmail.com>
2023-10-26 01:27:00 +0000

    add new route to display a specific url

--- express_server.js
+++ express_server.js
@@ -8,7 +8,13 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
