cf28e55183de03d8afa5da2ddebd374f67f120fe
svaronc <sebastianvaron96@gmail.com>
2023-10-26 02:54:43 +0000

    set the server to use express built in middleware and set the server to handle the post request

--- express_server.js
+++ express_server.js
@@ -2,16 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+function generateRamdomStrings() {
+  
+}
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.use(express.urlencoded({ extended: true }));
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
