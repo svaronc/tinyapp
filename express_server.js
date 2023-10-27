eea0663e0715a7b908e95ace4832d43618a752b9
svaronc <sebastianvaron96@gmail.com>
2023-10-27 00:39:05 +0000

    add post request to delete and redirect the urls

--- express_server.js
+++ express_server.js
@@ -37,6 +37,10 @@ app.post("/urls", (req, res) => {
   console.log(urlDatabase);
   res.redirect(`/urls/${shortid}`);
 });
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
